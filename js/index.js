05888149e7b4a2fe5f088b3a753e0e9a45fdbdd9
Kaushik Ganguly <kganguly@gmail.com>
2017-07-24 09:54:02 -0400

    Slider now active.;

---
+++ js/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    setListeners();
+
+});
+
+function setListeners() {
+    $(".meter input").click(function () {
+        var setTo = this.getAttribute("id");
+        console.log(setTo);
+        switch (setTo) {
+            case "setTo1":
+                var x = "-140";
+                var width = "12.5";
+                break;
+            case "setTo2":
+                var x = "-45";
+                var width = "37.5";
+                break;
+            case "setTo3":
+                var x = "45";
+                var width = "62.5";
+                break;
+            case "setTo4":
+                var x = "140";
+                var width = "87.5";
+        }
+        $(".slider").css("left", x + "px");
+        $(".mercury").css("width", width + "%");
+        $(".meter>label").removeClass("checked");
+        $(".meter input").removeAttr("checked");
+        this.setAttribute("checked", "checked");
+        this.parentElement.setAttribute("class", "checked");
+    })
+}
