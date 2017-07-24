9f7fd0cb7e69468bbfeb5fdd6bc8d3d0571006aa
Kaushik Ganguly <kganguly@gmail.com>
2017-07-24 19:12:22 -0400

    Aligned mercury and slider with fieldset labels.

--- js/index.js
+++ js/index.js
@@ -9,19 +9,19 @@ function setListeners() {
         console.log(setTo);
         switch (setTo) {
             case "setTo1":
-                var x = "-37.5";
-                var width = "12.5";
+                var x = "-36.5";
+                var width = "13";
                 break;
             case "setTo2":
-                var x = "-12.5";
+                var x = "-12";
                 var width = "37.5";
                 break;
             case "setTo3":
-                var x = "12.5";
+                var x = "12";
                 var width = "62.5";
                 break;
             case "setTo4":
-                var x = "37.5";
+                var x = "36.5";
                 var width = "87.5";
         }
         $(".slider").css("left", x + "%");
